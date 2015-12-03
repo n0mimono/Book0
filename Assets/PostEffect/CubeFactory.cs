39ae4f4752ebf57f6420bd9425e6f8d7bf687275
luke2 <r15yokote@gmail.com>
2015-12-03 22:58:26 +0900

    PostEffectを追加

---
+++ Assets/PostEffect/CubeFactory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeFactory : MonoBehaviour {
+	public Transform parent;
+
+	[ContextMenu("Create Cube")]
+	public void CreateCube() {
+		GameObject go = GameObject.CreatePrimitive (PrimitiveType.Cube);
+
+		float x = (2f * Random.value - 1f) * transform.localScale.x / 2f;
+		float y = (1f * Random.value - 0f) * transform.localScale.y / 1f;
+		float z = (2f * Random.value - 1f) * transform.localScale.z / 2f;
+		go.transform.position = new Vector3 (x, y, z);
+		go.transform.SetParent (parent);
+
+	}
+
+	[ContextMenu("Create Cube 500")]
+	public void CreateCube100() {
+		for (int i = 0; i < 500; i++) {
+			CreateCube ();
+		}
+	}
+}
---
+++ Assets/PostEffect/PostEffectManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PostEffectManager : MonoBehaviour {
+
+	[System.Serializable]
+	public class EffectMaterial {
+		public Material material;
+		public bool isEnabled;
+	}
+	public List<EffectMaterial> materials;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dst) {
+		if (!materials.Any (m => m.isEnabled)) {
+			Graphics.Blit (src, dst);
+			return;
+		}
+
+		materials
+			.Where (m => m.isEnabled)
+			.Select (m => m.material)
+			.ToList ().ForEach (m => Graphics.Blit (src, dst, m));
+	}
+}
