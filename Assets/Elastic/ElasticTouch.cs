7157d75e166b8749a16e140bb5635587a3e5927e
luke2 <r15yokote@gmail.com>
2015-12-06 17:17:44 +0900

    Elastic修正

---
+++ Assets/Elastic/ElasticTouch.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ElasticTouch : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler {
+	public RectTransform touchEffectRect;
+	public GameObject touchEffect;
+
+	public float stretchScale;
+
+	private class TouchStatus {
+		public Vector2 start;
+		public Vector2 end;
+
+		public Vector2 Dir { get { return (end - start).normalized; } }
+		public float   Mag { get { return (end - start).magnitude; } }
+	}
+	private TouchStatus st = new TouchStatus ();
+
+	private class EffectStatus {
+		public float alpha;
+		public float targetAlpha;
+
+		public void UpdateAlpha(float da) {
+			float dAlpha = targetAlpha - alpha;
+			if (Mathf.Abs (dAlpha) < da) {
+				alpha = targetAlpha;
+			} else {
+				alpha += Mathf.Sign (dAlpha) * da;
+			}
+		}
+		public void InitilizeAlpha() {
+			alpha = 0f;
+			targetAlpha = 0f;
+		}
+	}
+	private EffectStatus cur = new EffectStatus();
+
+	public void OnPointerDown (PointerEventData eventData) {
+		Vector2 pos = LocalPos (eventData.position);
+		touchEffectRect.localPosition = new Vector3(pos.x, pos.y, 0f);
+
+		st.start = pos;
+		SetTargetAlpha (1f);
+		SetStretch (0f);
+	}
+	public void OnPointerUp (PointerEventData eventData) {
+		SetTargetAlpha (0f);
+	}
+	public void OnDrag (PointerEventData eventData) {
+		Vector2 pos = LocalPos (eventData.position);
+
+		st.end = pos;
+		SetStretch (st.Mag * stretchScale);
+		SetDirection (st.Dir);
+	}
+
+	void Awake() {
+		Renderer effectRenderer = touchEffect.GetComponent<Renderer> ();
+		effectRenderer.sharedMaterial = Material.Instantiate (effectRenderer.material);
+		effectRenderer.sharedMaterial.hideFlags = HideFlags.HideAndDontSave;
+
+		StartCoroutine (AlphaSetter ());
+	}
+
+	private Vector2 LocalPos(Vector2 pos) {
+		RectTransform rectTrans = GetComponent<RectTransform> ();
+		Vector2 vec = Vector2.zero;
+		RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTrans, pos, Camera.main, out vec);
+		return vec;
+	}
+
+	[ContextMenu("Set Sorting Order Of Touch Effect")]
+	public void SetSortingOrderOfTouchEffect() {
+		Renderer effectRenderer = touchEffect.GetComponent<Renderer> ();
+		effectRenderer.sortingOrder = 1;
+	}
+
+	private void SetStretch(float strength) {
+		Renderer effectRenderer = touchEffect.GetComponent<Renderer> ();
+		effectRenderer.sharedMaterial.SetFloat ("_StretchStrength", strength);
+	}
+
+	private void SetDirection(Vector2 dir) {
+		Renderer effectRenderer = touchEffect.GetComponent<Renderer> ();
+		effectRenderer.sharedMaterial.SetVector ("_StretchDirection", new Vector4(dir.x, 0f, -dir.y, 1f));
+	}
+
+	private IEnumerator AlphaSetter() {
+		cur.InitilizeAlpha ();
+		ApplyCurrentAlpha ();
+
+		while (true) {
+			yield return true;
+
+			cur.UpdateAlpha (Time.deltaTime * 6f);
+			ApplyCurrentAlpha ();
+		}
+	}
+
+	private void ApplyCurrentAlpha() {
+		Renderer effectRenderer = touchEffect.GetComponent<Renderer> ();
+		effectRenderer.sharedMaterial.SetColor ("_Color", new Color(1f,1f,1f,cur.alpha)); // stupid thing
+	}
+
+	private void SetTargetAlpha(float alpha) {
+		cur.targetAlpha = alpha;
+	}
+
+	private void SetCurrentAlpha(float alpha) {
+		cur.alpha = alpha;
+	}
+
+}
+
