542280db9db10a4db631b2cbc7d30411cfe7d867
luke2 <r15yokote@gmail.com>
2015-12-12 21:18:51 +0900

    Vanillaシェーダ用エディタ拡張追加

---
+++ Assets/Vanilla/Editor/VanillaEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class VanillaEditor {
+
+	[MenuItem("Assets/Create/Shader/Lit Vanilla")]
+	public static void CreateLitVanilla() {
+		string curPath = Directory.GetCurrentDirectory ();
+		string dirPath = AssetDatabase.GetAssetPath (Selection.activeObject);
+		string filename = "VanillaLight.shader";
+		string srcPath = "Assets/Vanilla";
+
+		string srcFullpath = curPath + "/" + srcPath + "/" + filename;
+		string dstFullpath = curPath + "/" + dirPath + "/" + filename;
+
+		File.Copy (srcFullpath, dstFullpath);
+		AssetDatabase.Refresh ();
+	}
+
+}
