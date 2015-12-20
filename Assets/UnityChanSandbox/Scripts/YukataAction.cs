404e2043e5ff48f1653d4af2e6eecb04cc3dae41
luke2 <r15yokote@gmail.com>
2015-12-20 19:20:55 +0900

    差し替え

--- Assets/UnityChanSandbox/Scripts/YukataAction.cs
+++ Assets/UnityChanSandbox/Scripts/YukataAction.cs
@@ -10,28 +10,6 @@ public class YukataAction : MonoBehaviour {
 	public float maxSpeed;
 	public float animSpeedScale;
 
-	private Texture2D rimMask;
-
-	IEnumerator Start() {
-		
-		rimMask = new Texture2D(1, 1, TextureFormat.ARGB32, false);
-		rimMask.SetPixel (0, 0, Color.black);
-		rimMask.Apply ();
-		yield return null;
-
-		foreach (Renderer renderer in GetComponentsInChildren<Renderer> ()) {
-			Material material = renderer.sharedMaterial;
-
-			Material tmpMaterial = Material.Instantiate (material);
-			tmpMaterial.SetFloat ("_EdgeThickness", 0f);
-			tmpMaterial.SetTexture("_RimLightSampler", rimMask);
-
-			renderer.sharedMaterial = tmpMaterial;
-			yield return null;
-		}
-
-	}
-
 	public void SetTargetVelocity(Vector3 tgtVelocity) {
 		tgtVelocity = tgtVelocity.normalized * Mathf.Min (maxSpeed, tgtVelocity.magnitude);
 		if (tgtVelocity.magnitude < 0.5f) {
