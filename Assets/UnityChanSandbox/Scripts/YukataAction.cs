7c518c7104bd7ff6d55d25be89c8e8b821f4867f
luke2 <r15yokote@gmail.com>
2015-12-30 15:59:10 +0900

    Yukata微修正
    ・マテリアルPBSをモバイル対応
    ・走行速度を修正

--- Assets/UnityChanSandbox/Scripts/YukataAction.cs
+++ Assets/UnityChanSandbox/Scripts/YukataAction.cs
@@ -8,16 +8,17 @@ public class YukataAction : MonoBehaviour {
 	public Animator animator;
 
 	public float maxSpeed;
+	public float charSpeedScale;
 	public float animSpeedScale;
 
 	public void SetTargetVelocity(Vector3 tgtVelocity) {
 		tgtVelocity = tgtVelocity.normalized * Mathf.Min (maxSpeed, tgtVelocity.magnitude);
-		if (tgtVelocity.magnitude < 0.5f) {
+		if (tgtVelocity.magnitude < 0.2f) {
 			tgtVelocity = Vector3.zero;
 		}
 
 		// position
-		characterControl.Move (tgtVelocity * Time.deltaTime);
+		characterControl.Move (charSpeedScale * tgtVelocity * Time.deltaTime);
 
 		// angle
 		if (tgtVelocity.magnitude > 0.1f) {
