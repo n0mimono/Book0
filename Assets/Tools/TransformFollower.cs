3e3d4cb30223021d0e01056af9536ae0c494938a
luke2 <r15yokote@gmail.com>
2016-01-11 11:02:31 +0900

    Enchant修正

---
+++ Assets/Tools/TransformFollower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformFollower : MonoBehaviour {
+	public Transform parent;
+
+	public bool followPosition;
+	public bool followAngles;
+	public bool followScales;
+	public bool followActive;
+
+	void Update() {
+		if (followPosition) {
+			transform.position = parent.position;
+		}
+		if (followAngles) {
+			transform.eulerAngles = parent.eulerAngles;
+		}
+		if (followScales) {
+			transform.localScale = parent.localScale;
+		}
+		if (followActive) {
+			gameObject.SetActive (parent.gameObject.activeInHierarchy);
+		}
+
+	}
+
+}
