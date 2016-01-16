7f2c9c50bf77c138da0fa427b0c73fc23e37e7f0
luke2 <r15yokote@gmail.com>
2016-01-16 14:44:55 +0900

    ダメージ時表情追加

--- Assets/UnityChanSandbox/Scripts/UnityChanFaceSD.cs
+++ Assets/UnityChanSandbox/Scripts/UnityChanFaceSD.cs
@@ -7,15 +7,20 @@ public class UnityChanFaceSD : StateMachineBehaviour {
 		Loop    = 00,
 		Salute  = 01,
 		Running = 02,
+		Damage  = 03,
 		Idle    = 10,
 	}
 
 	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		if (stateInfo.IsFullName("Base.Lock Action.Salute")) {
+		if (stateInfo.IsFullName ("Base.Lock Action.Salute")) { // salute in
 			animator.SetFace ((int)FaceType.Salute);
-		} else if (stateInfo.IsFullName("Face.Salute")) {
+		} else if (stateInfo.IsFullName ("Face.Salute")) { // salute out
 			animator.SetFace ((int)FaceType.Loop);
-		} else if (stateInfo.IsFullName("Face.Random")) {
+		} else if (stateInfo.IsFullName ("Base.Lock Action.GoDown")) { // damage in
+			animator.SetFace ((int)FaceType.Damage);
+		} else if (stateInfo.IsFullName ("Face.Damage")) { // damage out
+			animator.SetFace ((int)FaceType.Loop);
+		} else if (stateInfo.IsFullName ("Face.Random")) {
 			int id = (int)FaceType.Idle + (int)(Random.value * 4f) + 1;
 			animator.SetFace (id);
 		} else {
