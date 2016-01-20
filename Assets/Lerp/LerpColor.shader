0d6b6b4f9fac151a79429043cfe8875dc56055b5
luke2 <r15yokote@gmail.com>
2016-01-21 00:08:32 +0900

    lerp追加

---
+++ Assets/Lerp/LerpColor.shader
@@ -0,0 +1,32 @@
+﻿Shader "Lerp/Color" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_Color ("Color", Color) = (1,1,1,1)
+		_LerpRate ("Lerp Rate", Range(0,1)) = 0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 150
+
+		CGPROGRAM
+		#pragma surface surf Lambert noforwardadd
+
+		sampler2D _MainTex;
+		float4 _Color;
+		float _LerpRate;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		void surf (Input IN, inout SurfaceOutput o) {
+		    fixed t = saturate(_LerpRate);
+			fixed4 c = lerp(tex2D(_MainTex, IN.uv_MainTex), _Color, t);
+			o.Albedo = c.rgb;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	}
+
+	Fallback "Mobile/VertexLit"
+}
---
+++ Assets/Lerp/Petrifier.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Custom;
+
+public class Petrifier : MonoBehaviour {
+	public Material material;
+	public List<Behaviour> exceptions;
+	public float time;
+
+	private List<Material> materials;
+	private bool isPetrified;
+	private List<Behaviour> behavs;
+
+	void Start() {
+		materials = new List<Material> ();
+
+		foreach (Renderer ren in GetComponentsInChildren<Renderer> ()) {
+			Material[] mats = ren.materials;
+			for (int i = 0; i < mats.Length; i++) {
+				Material mat = Instantiate (material);
+				mat.hideFlags = HideFlags.DontSave;
+				mat.SetTexture("_MainTex", mats[i].GetTexture("_MainTex"));
+				mats [i] = mat;
+
+				if (!materials.Where (m => m == mat).Any ()) {
+					materials.Add (mat);
+				}
+			}
+			ren.materials = mats;
+		}
+
+		foreach (SkinnedMeshRenderer ren in GetComponentsInChildren<SkinnedMeshRenderer> ()) {
+			ren.gameObject.AddComponent<MeshExploder> ();
+		}
+
+	}
+
+	public void Petrify() {
+		if (isPetrified) return;
+		isPetrified = true;
+
+		LerpMaterial (time).StartBy (this);
+	}
+
+	private IEnumerator LerpMaterial(float time) {
+		float t = 0f;
+		while (t < time) {
+			t += Time.deltaTime / time;
+			materials.ForEach (m => m.SetFloat ("_LerpRate", t));
+			yield return null;
+		}
+
+		behavs = GetComponentsInChildren<Behaviour> ()
+			.Where (b => b.enabled)
+			.Where (b => !exceptions.Where (e => e == b).Any ())
+			.ToList ();
+
+		behavs.ForEach (b => b.enabled = false);
+		yield return null;
+	}
+
+	public void Restore() {
+		if (!isPetrified) return;
+		isPetrified = false;
+
+		LerpMaterialRestore (time).StartBy (this);
+	}
+
+	private IEnumerator LerpMaterialRestore(float time) {
+		float t = 0f;
+		while (t < time) {
+			t += Time.deltaTime / time;
+			materials.ForEach (m => m.SetFloat ("_LerpRate", 1 - t));
+			yield return null;
+		}
+
+		behavs.ForEach (b => b.enabled = true);
+		yield return null;
+	}
+
+	public void Explosion() {
+		BroadcastMessage("Explode");
+		gameObject.SetActive (false);
+	}
+
+}
