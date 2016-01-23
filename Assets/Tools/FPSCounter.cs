1641cafd471299310ddffef8a2ce90d5f0e6a65d
luke2 <r15yokote@gmail.com>
2016-01-23 23:18:18 +0900

    fps追加

---
+++ Assets/Tools/FPSCounter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Custom;
+
+public class FPSCounter : MonoBehaviour {
+	public Text text;
+	public float fps;
+
+	void Update() {
+		fps = Time.timeScale / Time.deltaTime;
+
+		if (text != null) {
+			text.text = fps.ToString ("F1");
+		}
+	}
+}
