a5d0481a455eb14894fe3f1d48ca2eea1427c02c
luke2 <r15yokote@gmail.com>
2016-01-24 19:59:17 +0900

    angel途中まで

---
+++ Assets/Enchantress/Scripts/AngelBulletTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AngelBulletTrigger : MagicProjectile {
+
+	public override void Initialize() {
+		base.Initialize ();
+	}
+
+	public override void Load() {
+		base.Load ();
+	}
+
+	public override void Fire() {
+	}
+
+	public override void Unload() {
+		base.Unload ();
+	}
+
+}
